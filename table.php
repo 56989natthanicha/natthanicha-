702f08465342276f4a0619da8f63386677945d97
56989natthanicha <101694334+56989natthanicha@users.noreply.github.com>
2022-03-16 15:07:06 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>คิม แทฮยอง</td>
+        <td>วี </td>
+        <td><img src="https://pin.it/1QcGLvc" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td>
+      <tr>
+        <td>2</td>
+        <td>จอน จองกุก</td>
+        <td>จองกุก</td>
+        <td><img src="https://pin.it/s4Kqy4k" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td>
+      <tr>
+        <td>3</td>
+        <td>ปาร์ค จีมิน</td>
+        <td>จีมิน</td>
+        <td><img src="https://pin.it/2PPGmMV" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td>
+      <tr>
+        <td>4</td>
+        <td>จองโฮซอก</td>
+        <td>เจโฮป</td>
+        <td><img src="https://pin.it/3XO9GG6%E0%B8%97%E0%B8%A2%E0%B9%8C2.jpg" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td>
+       <tr>
+        <td>5</td>
+        <td>มิน ยุนกิ</td>
+        <td>ชูก้า</td>
+        <td><img src="https://pin.it/J7C7NoU" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td> 
+       <tr>
+        <td>6</td>
+        <td>คิม นัมจุน</td>
+        <td>rm</td>
+        <td><img src="https://pin.it/7rrJLuZ" width="145px"></td>
+        <td><a href="https://twitter.com/BTS_jp_official?t=eKqt1FUrJLUgGzs78mQTNA&s=09">twitter</a></td> 
+        </tr>  
+    </table>
+  </body> 
+</html>
