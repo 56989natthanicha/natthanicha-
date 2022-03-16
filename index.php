883ad52021fe662cbe198d6f7486ee93fa6caebf
56989natthanicha <101694334+56989natthanicha@users.noreply.github.com>
2022-03-16 13:18:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: blue;
+			}
+			
+			body{
+				background-color: pink;
+			}
+			h1{
+				color:yellow;
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>น.ส.ณัฏฐณิชา บุบผา เลขที่ 11 ม.4/13</h1>
+		</div>
+	</body>
+</html>
