b1bedb7b028c4ad5766e12582c538790f95dbf20
56989natthanicha <101694334+56989natthanicha@users.noreply.github.com>
2022-03-17 11:34:51 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(Doughnut.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(FCFlexica[Non-commercial]-Bold.woff)
+    }
+    #font1{
+	font-family: myFont1;    
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวณัฏฐณิชา บุบผา ม.4/13 เลขที่ 11 เลขประจำตัว 56989</h1>
+  <h1 id="font2">นางสาวณัฏฐณิชา บุบผา ม.4/13 เลขที่ 11 เลขประจำตัว 56989</h1>
+</body>
+</html>
